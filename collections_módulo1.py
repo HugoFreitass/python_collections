4bdc6395e207a6d10bbd887aca9206eb81c3d0bd
HugoFreitass <videogame041@gmail.com>
2023-04-20 21:01:30 -0300

    implementando método abstrato

--- collections_módulo1.py
+++ collections_módulo1.py
@@ -1,24 +1,45 @@
-class ContaCorrente:
+from abc import ABCMeta, abstractmethod
+
+class Conta(metaclass = ABCMeta):
 
     def __init__(self, codigo):
-        self.codigo = codigo
-        self.saldo = 0
+        self._codigo = codigo
+        self._saldo = 0
 
     def deposita(self, valor):
-        self.saldo += valor
+        self._saldo += valor
+
+    @abstractmethod
+    def passa_o_mes(self):
+        pass
 
     def __str__(self):
-        return "[>> Código: {} Saldo: {} <<]".format(self.codigo, self.saldo)
+        return "[>> Código: {} Saldo: {} <<]".format(self._codigo, self._saldo)
+
+
+
+class ContaCorrente(Conta):
+    def passa_o_mes(self):
+        self._saldo -= 2
+
+
+class ContaPoupanca(Conta):
+    def passa_o_mes(self):
+        self._saldo *= 1.01
+        self._saldo -= 3
 
 
 conta_hugo = ContaCorrente(2505)
 print(conta_hugo)
 
-conta_geovana = ContaCorrente(6543)
+conta_geovana = ContaPoupanca(6543)
 conta_geovana.deposita(1000)
 print(conta_geovana)
+
 
 contas = [conta_geovana, conta_hugo]
 
 for conta in contas:
+    conta.passa_o_mes() #duck typing
     print(conta)
+    
