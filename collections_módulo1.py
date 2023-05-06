bdb1535272428fb7c85e1009c6d22eaa81e77ef2
HugoFreitass <videogame041@gmail.com>
2023-05-06 01:00:55 -0300

    total_ordering

--- collections_módulo1.py
+++ collections_módulo1.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta, abstractmethod
+from functools import total_ordering
 
 class Conta(metaclass = ABCMeta):
 
@@ -17,11 +18,9 @@ class Conta(metaclass = ABCMeta):
         return "[>> Código: {} Saldo: {} <<]".format(self._codigo, self._saldo)
 
 
-
 class ContaCorrente(Conta):
     def passa_o_mes(self):
         self._saldo -= 2
-
 
 
 class ContaPoupanca(Conta):
@@ -30,6 +29,7 @@ class ContaPoupanca(Conta):
         self._saldo -= 3
 
 
+@total_ordering
 class ContaSalario:
     def __init__(self, codigo):
          self._codigo = codigo
@@ -42,13 +42,16 @@ class ContaSalario:
         return self._codigo == outro._codigo and self._saldo == outro._saldo
     
     def __lt__(self, outro):
-        return self._saldo < outro._saldo
-    
+        if self._saldo != outro._saldo:
+            return self._saldo < outro._saldo
+        return self._codigo < outro._codigo
+
     def deposita(self, valor):
         self._saldo += valor
 
     def __str__(self):
         return "[Código {}, Saldo {}]".format(self._codigo, self._saldo)
+
 
 conta_hugo = ContaCorrente(2505)
 print(conta_hugo)
@@ -57,7 +60,6 @@ conta_geovana = ContaPoupanca(6543)
 conta_geovana.deposita(1000)
 print(conta_geovana)
 
-
 contas = [conta_geovana, conta_hugo]
 
 for conta in contas:
@@ -65,13 +67,13 @@ for conta in contas:
     print(conta)
 
 
-conta_funcionario1 = ContaSalario(170)
-conta_funcionario2 = ContaSalario(133)
-conta_funcionario3 = ContaSalario(245)
+conta_funcionario1 = ContaSalario(5000)
+conta_funcionario2 = ContaSalario(1223)
+conta_funcionario3 = ContaSalario(2450)
 
 conta_funcionario1.deposita(2000)
 conta_funcionario2.deposita(1250)
-conta_funcionario3.deposita(3500)
+conta_funcionario3.deposita(3000)
 
 contas_salario = [conta_funcionario1, conta_funcionario2, conta_funcionario3]
 
