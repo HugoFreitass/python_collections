2fa0c8f2731f066039f551bbf9865ed817269bf4
HugoFreitass <videogame041@gmail.com>
2023-05-12 19:11:44 -0300

    Usando o Counter

--- collections_módulo2.py
+++ collections_módulo2.py
@@ -1,13 +1,9 @@
-from collections import defaultdict
+from collections import Counter
 
 meu_texto = "Olá eu sou o ChatGPT um modelo de linguagem criado pela OpenAI para ajudá-lo com suas necessidades sou o melhor no que faço fui criado em python usando um modelo de aprendizagem de máquina."
 
 meu_texto = meu_texto.lower()
 
-aparicoes = defaultdict(int)
-
-for palavra in meu_texto.split():
-    aparicoes[palavra] += 1
-
+aparicoes = Counter(meu_texto.split())
 
 print(aparicoes)
